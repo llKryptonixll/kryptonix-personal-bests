1af08b63b1f950c32f5fdcf6041250fa317fd95b
Lucas Cerri <lucascerri3@outlook.de>
2022-07-09 18:13:37 +0200

    map stats created with functions

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ selectGame_BTN.forEach((btn) => {
     });
 });
 
-// close map selection
 
+// close map selection
 closeMapSelection = () => {
     mainContainer.style.display = "grid";
 
@@ -29,5 +29,56 @@ const closeBtns = document.querySelectorAll(".close-map-selection");
 closeBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
         closeMapSelection();
+    });
+});
+
+
+// open map stats
+const selectMap_BTN = document.querySelectorAll(".maps");
+
+selectMap_BTN.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const mapStatsData = btn.getAttribute("data-maps");
+        const mapStats = document.getElementById(mapStatsData);
+
+        console.log(mapStats)
+
+        if(mapStats == null){
+            alert("not attempted or not added")
+        }
+        else{
+            mapStats.classList.add("show-gamestats-JS");
+            mapStats.classList.remove("hide-gamestats-JS");
+        }
+    
+    });
+});
+// close map stats
+const closeMapsStats_BTN = document.querySelectorAll(".close-map-stats");
+
+closeMapStats = () => {
+    const mapStats = document.querySelectorAll(".game-stats-container");
+
+    mapStats.forEach((section) => {
+        section.classList.add("hide-gamestats-JS");
+    });
+}
+
+closeMapsStats_BTN.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        closeMapStats();
+    });
+});
+
+
+// accordion function
+const openAccordion_BTN = document.querySelectorAll(".open-acc-panel");
+
+openAccordion_BTN.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const accordionData = btn.getAttribute("data-accordion");
+        const accordion = document.getElementById(accordionData);
+
+        accordion.classList.toggle("open-accordion-JS");
     });
 });
